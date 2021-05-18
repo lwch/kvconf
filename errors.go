e32a4b10d8cb2da24bfa99bd73242729046fdfe2
lwch <lwch748@gmail.com>
2021-05-18 13:39:43 +0800

    增加注释

--- errors.go
+++ errors.go
@@ -2,10 +2,12 @@ package kvconf
 
 import "reflect"
 
+// UnsupportedTypeError error for unsupported type
 type UnsupportedTypeError struct {
 	Type reflect.Type
 }
 
+// Error error message
 func (e *UnsupportedTypeError) Error() string {
 	return "kvconf: unsupported type: " + e.Type.String()
 }
